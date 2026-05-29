ed278f588c5a3ac6152fbb4924331b987be08074
iron-vertical <125842202+iron-vertical@users.noreply.github.com>
2026-05-29 00:52:00 -0400

    spelling correction

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "blog" {
   name = "blog"
   description = "Allow http and hssps in. Allow everything out"
 
-  vps_id = data.aws_vpc.default.id
+  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
