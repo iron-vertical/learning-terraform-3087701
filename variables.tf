460780fc0b06c4a379eb6191f3fe973c412371c5
iron-vertical <125842202+iron-vertical@users.noreply.github.com>
2026-05-29 23:57:14 -0400

    refactored with variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,38 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.micro"
 }
+
+variable "ami_filter" {
+  description = "variable ami_filter"
+  type = object({
+    name = string
+    owner = string
+  })
+
+  default = {
+    name   = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
+}
+
+variable "environment" {
+  description = "variable environment"
+  type = object ({
+    name = string
+    network_prefix = string
+  })
+  default = {
+    name = "dev"
+    network_prefix = "10.0"
+  }
+}
+
+variable "min_size" {
+  description = "variable min_size"
+  default = 1
+}
+
+variable "max_size" {
+  description = "variable max_size"
+  default = 2
+}
+
